d0823426780aed16d6ce1251474821711e14c585
Umut <umut.tanriverdiceng@gmail.com>
2023-06-07 15:21:38 +0300

    MaviYaka Class'ı Setter Getter Methodları

--- MaviYaka.py
+++ MaviYaka.py
@@ -5,5 +5,10 @@ class MaviYaka(Calisan):
         super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas)
         self.__yipranma_payi = yipranma_payi
 
+    def get_yipranma_payi(self):
+        return self.__yipranma_payi
+    def set_yipranma_payi(self, yipranma_payi):
+        self.__yipranma_payi = yipranma_payi
+
     
 
