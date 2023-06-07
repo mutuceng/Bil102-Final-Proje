13d45d007d3350fabb3bd115275e5f7b652ac840
Umut <umut.tanriverdiceng@gmail.com>
2023-06-07 15:32:05 +0300

    BeyazYaka Class'ı Setter Getter Methodları

--- BeyazYaka.py
+++ BeyazYaka.py
@@ -5,4 +5,9 @@ class BeyazYaka(Calisan):
         super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas)
         self.__tesvik_primi = tesvik_primi
 
+    def get_tesvik_primi(self):
+        return self.__tesvik_primi
+    def set_tesvik_primi(self, tesvik_primi):
+        self.__tesvik_primi = tesvik_primi
+
     
