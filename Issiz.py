ca080af4eb196b60887870898160ba778336681f
Umut <umut.tanriverdiceng@gmail.com>
2023-06-07 15:01:48 +0300

    Issiz Classı Setter Getters Method

--- Issiz.py
+++ Issiz.py
@@ -3,5 +3,9 @@ class Issiz(Insan):
     def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, tecrube_dict):
         super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk)
         self.__tecrube_dict = tecrube_dict
+    def get_tecrube_dict(self):
+        return self.__tecrube_dict
+    def set_tecrube_dict(self, tecrube_dict):
+        self.__tecrube_dict = tecrube_dict
 
     
