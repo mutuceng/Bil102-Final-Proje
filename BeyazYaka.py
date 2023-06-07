2a802704f0b55c11bb4839f9be52097e574e61ce
Umut <umut.tanriverdiceng@gmail.com>
2023-06-07 15:33:01 +0300

    BeyazYaka Class'ı zam_hakki Fonksiyonu

--- BeyazYaka.py
+++ BeyazYaka.py
@@ -10,4 +10,20 @@ class BeyazYaka(Calisan):
     def set_tesvik_primi(self, tesvik_primi):
         self.__tesvik_primi = tesvik_primi
 
+    def zam_hakki(self):
+        try:
+            tecrube = int(self.get_tecrube())
+            maas = float(self.get_maas())
+            tesvik_primi = float(self.__tesvik_primi)
+            if tecrube < 2:
+                return tesvik_primi
+            elif tecrube >= 2 and tecrube <= 4 and maas < 15000:
+                return (maas * tecrube / 100) * 5 + tesvik_primi
+            elif tecrube > 4 and maas < 25000:
+                return (maas * tecrube / 100) * 4 + tesvik_primi
+            else:
+                return 0
+        except ValueError:
+            return 0
+
     
