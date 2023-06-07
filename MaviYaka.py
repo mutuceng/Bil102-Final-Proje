a05fd6239528bbced215a2f102d6c54b4949e6c1
Umut <umut.tanriverdiceng@gmail.com>
2023-06-07 15:22:24 +0300

    MaviYaka Class'ı zam_hakki fonksiyonu

--- MaviYaka.py
+++ MaviYaka.py
@@ -10,5 +10,19 @@ class MaviYaka(Calisan):
     def set_yipranma_payi(self, yipranma_payi):
         self.__yipranma_payi = yipranma_payi
 
-    
+    def zam_hakki(self):
+        try:
+            tecrube = int(self.get_tecrube())
+            maas = float(self.get_maas())
+            yipranma_payi = float(self.__yipranma_payi)
+            if tecrube < 2:
+                return yipranma_payi * 10
+            elif tecrube >= 2 and tecrube <= 4 and maas < 15000:
+                return (maas * tecrube / 100) / 2 + (yipranma_payi * 10)
+            elif tecrube > 4 and maas < 25000:
+                return (maas * tecrube / 100) / 3 + (yipranma_payi * 10)
+            else:
+                return 0
+        except ValueError:
+            return 0
 
